76c8527f16462f7aabbb568e8aca0657b36a3478
JoshuaRTucker <joshtuckerapps@gmail.com>
2014-03-29 18:45:42 -0400

    changed everything to php

--- client/vote.php
+++ client/vote.php
@@ -35,29 +35,8 @@
   </head>
   <body>
 
-  <nav class="navbar navbar-default navbar-fixed-top navbar-absolute navbar-color" role="navigation">
-    <div class="navbar-header">
 
-    <div class="col-xs-2 col-md-3 logo-style-index logo-width">
-        <img src="img/LogoLeft.png" alt="logo">
-    </div>
-
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">Pocket Change</a>
-    </div>
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-    <ul class="nav navbar-nav">
-      <li><a href="#">My Stats</a></li>
-      <li><a href="#">Achievements</a></li>
-      <li><a href="about.html">About</a></li>
-    </ul>
-    </div>
-  </nav>
+<?php include 'navbar.php'; ?>
 
   <!-- Carousel  -->
 
