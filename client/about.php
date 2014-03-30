cb496e2fa42d342884f84506d525bba4f1017b0f
ChrisBarthol <Chris.Barthol@gmail.com>
2014-03-29 20:13:29 -0400

    Added about text

--- client/about.php
+++ client/about.php
@@ -51,7 +51,10 @@
     <div class="about_text center">
       <h1> Pocket Change </h1>
 
-      <p> Some about text </p>
+      <p> Pocket Change is a micro-donation app that helps you support a number of non-profits, no matter how much you have to give. With Pocket Change, you set the amount you’re able to give every month, to as low as $5. From there, you can discover new charities through our easy to use swiping interface. Each time you “give” to a charity, you are supporting their cause.</p>
+
+<p>At the end of the month, we divide your monthly donation amount by the number of charities you contributed to during that month and pass your money along to those causes. The lowest amount you can contribute is 10 cents. So if you have $5 to spare, you could end up supporting 50 non-profits in one month!
+ </p>
 
       <p> Pocket Change was founded at Columbus Startup Weekend by Iam Williams, Jake Bowles, Chris Barthol, Joshua Tucker.</p>
     </div>
